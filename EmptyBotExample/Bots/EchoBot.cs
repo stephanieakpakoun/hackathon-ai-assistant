5f1f20df933196bacd5015e8ba33412091856957
Stephanie Akpakoun <stephanie.akpakoun@submittable.com>
2023-07-11 14:18:28 -0500

    Adding new form bot called emptyBot

---
+++ EmptyBotExample/Bots/EchoBot.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with EchoBot .NET Template version v4.17.1
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Net.Http;
+using System.Text;
+
+namespace Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = await CreateFormAsync();
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+        
+        private async Task<string> CreateFormAsync()
+        {
+            string uri = "https://submittable-api.submittable.com/v3/forms";
+            // TODO: Get apiKey from env
+            var encodedKey = Base64Encode("API_KEY_HERE");
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(uri),
+                Headers =
+                {
+                    { "Authorization", $"Basic YjU2MzY5OWEyNDZiNDUyY2I3MTM5ZGU5OWY4YTk5OTA6" }
+                },
+            };
+
+            var date = DateTime.Now;
+            string postBody =  "{'name': 'A bot made this form at: " + date + "','formType': 'initial', 'fields': [{ 'fieldType':'title','label': 'Title','isRequired': true }]}";
+            
+            request.Content = new StringContent(postBody, Encoding.UTF8, "application/json");
+            
+            
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return body;
+            }
+        }
+        
+        
+        public static string Base64Encode(string plainText) 
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
--- EmptyBotExample/Startup.cs
+++ EmptyBotExample/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Runtime.Extensions;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Hosting;
 
 namespace EmptyBotExample
@@ -25,6 +27,14 @@ namespace EmptyBotExample
                 options.SerializerSettings.MaxDepth = HttpHelper.BotMessageSerializerSettings.MaxDepth;
             });
             services.AddBotRuntime(Configuration);
+            // Create the Bot Framework Authentication to be used with the Bot Adapter.
+            services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+            // Create the Bot Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, Bots.AdapterWithErrorHandler>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, Bots.EchoBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
